ac38926a987b670bd590608863c390e7be0e0f4a
Nikit Srivastava <nikit91@gmail.com>
2022-06-23 17:05:38 +0200

    adding logs and handling empty response

--- backend/src/main/java/org/dice/porque/qasystems/QanaryQA.java
+++ backend/src/main/java/org/dice/porque/qasystems/QanaryQA.java
@@ -1,12 +1,17 @@
 package org.dice.porque.qasystems;
 
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.commons.io.IOUtils;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QuerySolution;
@@ -15,14 +20,13 @@ import org.dice.porque.constants.PORQUEConstant;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class QanaryQA implements QASystems {
+	
+	Logger logger = LoggerFactory.getLogger(QanaryQA.class);
 
     private final String qanaryUrl = PORQUEConstant.QANARY_URL;
     private final String sparqlEndpoint = PORQUEConstant.QANARY_SPARQL_ENDPOINT;
@@ -37,7 +41,10 @@ public class QanaryQA implements QASystems {
     public String getQALDresponse(String query, String lang, int pipeline) {
 
         JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        
         try {
+        	jsonObject.put("questions", jsonArray);
             String qanaryResponseOutGraph = getResponseQanaryPipeline(query, pipeline);
             System.out.println(qanaryResponseOutGraph);
             List<String> sparqlQuery = getSparqlQuery(qanaryResponseOutGraph);
@@ -47,7 +54,7 @@ public class QanaryQA implements QASystems {
             if (results.size() != 0) {
                 System.out.println(results.get(0));
 
-                JSONArray jsonArray = new JSONArray();
+                
                 for (QanaryResult qanaryResult : results) {
                     JSONArray bindings = new JSONArray();
                     for (String resultstr : qanaryResult.result) {
@@ -77,7 +84,7 @@ public class QanaryQA implements QASystems {
                                                     "bindings", bindings))));
                     jsonArray.put(question);
                 }
-                jsonObject.put("questions", jsonArray);
+                
             }
 
         } catch (JSONException e) {
@@ -173,7 +180,7 @@ public class QanaryQA implements QASystems {
                 break;
 
         }
-
+        logger.debug("Component List: "+ componentListJson);
 
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpPost httppost = new HttpPost(qanaryUrl);
@@ -188,7 +195,14 @@ public class QanaryQA implements QASystems {
                 // String result = getStringFromInputStream(instream);
                 String responsetext = IOUtils.toString(instream, StandardCharsets.UTF_8.name());
                 JSONObject responseJson = new JSONObject(responsetext);
-                outGraph = (String) responseJson.get("outGraph");
+                logger.debug("Qanary response: "+responseJson);
+                // Check for outgraph
+                if(responseJson.has("outGraph")) {
+                	outGraph = (String) responseJson.get("outGraph");
+                }
+                else {
+                	logger.error("Response from Qanary has no attribute named outgraph.");
+                }
             }
             // HttpEntity entity = response.toString();
         } catch (Exception e) {
