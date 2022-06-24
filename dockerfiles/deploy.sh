c65d90d81461b2806711304b04c1d0ac7208bba2
Nikit Srivastava <nikit91@gmail.com>
2022-06-24 18:06:55 +0200

    modifiying deployment workflow

--- dockerfiles/deploy.sh
+++ dockerfiles/deploy.sh
@@ -2,59 +2,45 @@
 
 cd "${BASH_SOURCE%/*}" || exit
 
-version=$(cat VERSION)
-registry=${REGISTRY:-localhost:5000}
+cd ..
+git submodule update --init
+git submodule update --recursive --remote
 
-function build_container() {
-    tag=$registry/porque/$1:$version
-    echo "Building $1: $tag..."
-    if docker build "${@:2}" -t "$tag"; then
-        echo "Successfully built $tag."
-        if docker push "$tag"; then
-            echo "Successfully pushed $tag."
-        fi
-    fi
-}
- cd ..
- git submodule update --init
- git submodule update --recursive --remote
- 
- # Starting Stardog server
- bash $STARDOG_HOME/stardog-admin server start
- 
- cd dockerfiles/
-	 
+# Starting Stardog server
+bash $STARDOG_HOME/stardog-admin server start
 
- cd ../external-qa/qanary/qanary-core/qanary_pipeline-template/
- mvn clean install -Ddockerfile.skip=false -DskipTests
+cd dockerfiles/
 
- cd ../../qanary-components/qanary_component-NED-DBpedia-Spotlight/
- mvn clean install -Ddockerfile.skip=false -DskipTests
- 
- cd ../qanary_component-REL-RelationLinker2/
- mvn clean install -Ddockerfile.skip=false -DskipTests
- 
- cd ../qanary_component-QB-Sina/
- mvn clean install -Ddockerfile.skip=false -DskipTests
- 
- cd ../qanary_component-NED-Falcon/
- mvn clean install -Ddockerfile.skip=false -DskipTests
- 
- cd ../qanary_component-NED-Falcon-Enriched/
- mvn clean install -Ddockerfile.skip=false -DskipTests
- 
- cd ../qanary_component-REL-RelationLinker3/
- mvn clean install -Ddockerfile.skip=false -DskipTests
- 
- cd ../qanary_component-QB-SQG/
- mvn clean install -Ddockerfile.skip=false -DskipTests
- 
- cd ../../../../dockerfiles/
- 
-build_container nginx .. -f nginx/Dockerfile
-build_container backend-server .. -f backend/Dockerfile
 
-export REGISTRY=$registry
-export VERSION=$version
+cd ../external-qa/qanary/qanary-core/qanary_pipeline-template/
+mvn clean install -Ddockerfile.skip=false -DskipTests
 
+cd ../../qanary-components/qanary_component-NED-DBpedia-Spotlight/
+mvn clean install -Ddockerfile.skip=false -DskipTests
+
+cd ../qanary_component-REL-RelationLinker2/
+mvn clean install -Ddockerfile.skip=false -DskipTests
+
+cd ../qanary_component-QB-Sina/
+mvn clean install -Ddockerfile.skip=false -DskipTests
+
+cd ../qanary_component-NED-Falcon/
+mvn clean install -Ddockerfile.skip=false -DskipTests
+
+cd ../qanary_component-NED-Falcon-Enriched/
+mvn clean install -Ddockerfile.skip=false -DskipTests
+
+cd ../qanary_component-REL-RelationLinker3/
+mvn clean install -Ddockerfile.skip=false -DskipTests
+
+cd ../qanary_component-QB-SQG/
+mvn clean install -Ddockerfile.skip=false -DskipTests
+
+cd ../../../../dockerfiles/
+ 
+cd nginx/
+docker build -t porque-nginx .
+cd ../backend
+docker build -t porque-backend .
+cd ..
 docker stack deploy --compose-file docker-compose.yml porque-stack
