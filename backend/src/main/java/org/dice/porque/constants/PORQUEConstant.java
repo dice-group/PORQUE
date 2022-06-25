1e79ccc7d7ed031c52e8551f01d0f253d9c199d8
Nikit Srivastava <nikit91@gmail.com>
2022-06-25 11:42:57 +0200

    modifying libretranslate endpoint

--- backend/src/main/java/org/dice/porque/constants/PORQUEConstant.java
+++ backend/src/main/java/org/dice/porque/constants/PORQUEConstant.java
@@ -11,7 +11,7 @@ public interface PORQUEConstant {
     String ENGLISH_LANG_CODE = "en";
 
     //API URL
-    String LIBRE_TRANSLATE_URL = "https://translate.astian.org/translate";
+    String LIBRE_TRANSLATE_URL = "http://porque.cs.upb.de:5310/translate";
     String TEBAQA_URL = "http://tebaqa.cs.upb.de:8080/qa-porque";
     String QANSWER_URL = "http://qanswer-core1.univ-st-etienne.fr/api/gerbil";
     String QANARY_URL = "http://porque.cs.upb.de:8888/startquestionansweringwithtextquestion";
