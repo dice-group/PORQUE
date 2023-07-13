5fe121268f1ac8f479749e6ac18bba7a6818f3a9
Nikit Srivastava <nikit91@gmail.com>
2023-07-13 14:56:23 +0200

    handling exceptions and empty answers for lfqa

--- frontend/main.py
+++ frontend/main.py
@@ -2,6 +2,7 @@
 import flask
 import json
 import requests
+import traceback
 
 neamt_api = 'http://porque.cs.upb.de/porque-neamt/custom-pipeline'
 poolparty_api = 'https://demos-02.poolparty.biz/'
@@ -24,28 +25,52 @@ lfqa_systems = {
 
 app = flask.Flask(__name__)
 
-
+def translate_message(lang, message):
+    retval = ''
+    if not lang or (lang == 'en'):
+        retval = message
+    else:
+        data = requests.post(neamt_api, data={
+            'query': message,
+            'components': 'libre_mt',
+            'full_json': 'true',
+            'target_lang': lang
+        }).json()
+        print(data)
+        query = data['translated_text']
+        retval = query
+    return retval
 
 def neamt_service(form):
     query = form['query']
-    data = requests.post(neamt_api, data={
-        'query': query,
-        'components': form['neamt_components'],
-        'full_json': 'true',
-    }).json()
+    data = {}
+    try:
+        data = requests.post(neamt_api, data={
+            'query': query,
+            'components': form['neamt_components'],
+            'full_json': 'true',
+        }).json()
 
-    query = data['translated_text']
-    # call the qa system
-    qa_sys = lfqa_systems[form['qa_system_id']]
+        query = data['translated_text']
+        # call the qa system
+        qa_sys = lfqa_systems[form['qa_system_id']]
 
-    qa_data = qa_sys['request_formatter'](query, qa_sys['uri'])
+        qa_data = qa_sys['request_formatter'](query, qa_sys['uri'])
 
-    data.update(qa_data)
-
-    return {
-        'answer': data['answer'],
-        'json': data,
-    }
+        data.update(qa_data)
+        print(data)
+        retval = {
+            'answer': data['answer'] if len(data['answer']) > 0 else translate_message(data.get('lang'),'No answer found.'),
+            'json': data
+        }
+    except Exception as err:
+        # printing stack trace
+        traceback.print_exc()
+        retval = {
+            'answer': translate_message(data.get('lang'), 'Something went wrong, please contact the system administrator.'),
+            'json': {'exception': '%s: %s'%(type(err).__name__, str(err))}
+        }
+    return retval
 
 def poolparty_service(form):
     assert form['poolparty_project_id'] in poolparty_projects
