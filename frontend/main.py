34899d33ed746844f3f6fce6effb7536b3130d9d
Nikit Srivastava <nikit91@gmail.com>
2024-01-03 14:46:47 +0100

    updating porque ui

--- frontend/main.py
+++ frontend/main.py
@@ -6,8 +6,28 @@ import requests
 import time
 import traceback
 
+
+from SPARQLWrapper import JSON
+from SPARQLWrapper import SPARQLWrapper
+from SPARQLWrapper.SPARQLExceptions import SPARQLWrapperException
+
 neamt_api = 'http://porque.cs.upb.de/porque-neamt/custom-pipeline'
 poolparty_api = 'https://demos-02.poolparty.biz/'
+
+porque_langs = {
+    'de': 'German',
+    'fr': 'French',
+    'es': 'Spanish',
+    'en': 'English'
+}
+
+sparql_endpoints = {
+    'dbpedia_2016-10_enriched': {'label': 'DBpedia 2016-10 (Enriched)', 'uri' : 'http://porque.cs.upb.de:8890/sparql/', 'named_graph' : 'http://www.upb.de/en-dbp2016-10-enriched'},
+    'dbpedia_2016-10_normal': {'label': 'DBpedia 2016-10', 'uri' : 'http://porque.cs.upb.de:8890/sparql/', 'named_graph' : 'http://www.upb.de/en-dbp2016-10-normal'},
+    'wikidata_qald10': {'label': 'Wikidata (QALD10 instance)',' uri' : 'https://skynet.coypu.org/wikidata/', 'named_graph' : None},
+    'wikidata': {'label': 'Wikidata (Latest)', 'uri' : 'https://query.wikidata.org/sparql', 'named_graph' : None},
+    'dbpedia': {'label': 'DBpedia (Latest)', 'uri' : 'https://dbpedia.org/sparql', 'named_graph' : None}
+}
 
 app = flask.Flask(__name__)
 
@@ -26,6 +46,22 @@ def fetch_poolparty_projects(ttl_hash=None):
 def get_poolparty_projects():
     return fetch_poolparty_projects(ttl_hash=ttl_hash())
 
+
+def send_sparql(sparql_str, endpoint_info):
+    try:
+        sparql = SPARQLWrapper(endpoint_info['uri'])
+        named_graph = sparql_endpoints.get('named_graph')
+        if named_graph:
+            sparql.setNamedGraph(named_graph)
+        sparql.setQuery(sparql_str)
+        sparql.setReturnFormat(JSON)
+        sparql_results = sparql.query().convert()
+        return sparql_results
+    except Exception as e:
+        print('Exception occurred for \tSPARQL: %s' % (sparql_str))
+        print(str(e))
+        return {"head": {"vars": []}, "results": {"bindings": []}}
+
 def qsw_request_formatter(question, uri):
     data = requests.get(uri, params={'question': question}).json()
     return {
@@ -33,11 +69,51 @@ def qsw_request_formatter(question, uri):
         'json': data,
     }
 
+
+def prettify_answers(answers_raw):
+    """
+    Adapted from: https://github.com/WSE-research/qa-systems-wrapper/blob/84f90d703ebf620162ab085015e2f436dda7ef1e/routers/qanswer.py#L16-L23
+    """
+    if 'results' in answers_raw.keys() and len(answers_raw['results']['bindings']) > 0:
+        # converting to set
+        res = set()
+        for uri in answers_raw['results']['bindings']:
+            res.add(uri[list(uri.keys())[0]]['value'])
+        return list(res)
+    else:
+        return []
+
+def mst5_request_formatter(question, uri):
+    payload = {
+        'query': question,
+        'lang': 'en'
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    sparql_str = requests.post(uri, headers=headers, data=payload).text
+    if 'wikidata' in uri:
+        # send sparql to wikidata
+        endpoint_info = sparql_endpoints['wikidata']
+    else:
+        # send sparql to dbpedia
+        endpoint_info = sparql_endpoints['dbpedia']
+
+    answer = send_sparql(sparql_str, endpoint_info)
+    
+    return {
+        'answer': prettify_answers(answer),
+        'sparql': sparql_str.replace("\n"," ")
+    }
+
+
 lfqa_systems = {
+    'mst5-wikidata': {'uri': 'http://porque.cs.upb.de/mst5/wikidata/fetch-sparql', 'request_formatter': mst5_request_formatter},
+    'mst5-dbpedia': {'uri': 'http://porque.cs.upb.de/mst5/dbpedia/fetch-sparql', 'request_formatter': mst5_request_formatter},
     'tebaqa': {'uri': 'http://141.57.8.18:40199/tebaqa/answer', 'request_formatter': qsw_request_formatter},
     'gAnswer': {'uri': 'http://141.57.8.18:40199/gAnswer/answer', 'request_formatter': qsw_request_formatter},
-    'deeppavlov': {'uri': 'http://141.57.8.18:40199/deeppavlov2023/answer', 'request_formatter': qsw_request_formatter},
-    'deeppavlov2.0': {'uri': 'http://141.57.8.18:40199/deeppavlov2023/answer', 'request_formatter': qsw_request_formatter}
+    'deeppavlov': {'uri': 'http://141.57.8.18:40199/deeppavlov/answer', 'request_formatter': qsw_request_formatter},
+    'deeppavlov2.0': {'uri': 'http://141.57.8.18:40199/deeppavlov2023/answer', 'request_formatter': qsw_request_formatter},
 }
 
 def translate_message(lang, message):
@@ -56,19 +132,84 @@ def translate_message(lang, message):
         retval = query
     return retval
 
-def neamt_service(form):
+def mst5_service(form):
     query = form['query']
+    lang = form['mst5_lang_id']
+    endpoint_id = form['mst5_sparql_endpoint_id']
+    endpoint_info = sparql_endpoints[endpoint_id]
+    try:
+        payload = {
+        'query': query,
+        'lang': lang
+        }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        mst5_uri = lfqa_systems['mst5-wikidata']['uri']
+        if 'dbpedia' in endpoint_id:
+            mst5_uri = lfqa_systems['mst5-dbpedia']['uri']
+        
+        sparql_str = requests.post(mst5_uri, headers=headers, data=payload).text
+
+        answer = send_sparql(sparql_str, endpoint_info)
+        
+        data = {
+            'answer': prettify_answers(answer),
+            'sparql': sparql_str.replace("\n"," ")
+        }
+        retval = {
+            'answer': data['answer'] if len(data['answer']) > 0 else translate_message(data.get('lang'),'No answer found.'),
+            'json': data
+        }
+    except Exception as err:
+        # printing stack trace
+        traceback.print_exc()
+        retval = {
+            'answer': translate_message(data.get('lang'), 'Something went wrong, please contact the system administrator.'),
+            'json': {'exception': '%s: %s'%(type(err).__name__, str(err))},
+            'exception': True
+        }
+    return retval
+    
+
+def neamt_service(form, arg2='neamt_'):
+    query = form['query']
+    form_prefix = arg2
+    lang = form[form_prefix + 'lang_id']
     data = {}
     try:
         data = requests.post(neamt_api, data={
             'query': query,
-            'components': form['neamt_components'],
+            'components': form[form_prefix + 'neamt_components'],
             'full_json': 'true',
+            'lang': lang
         }).json()
+        retval = {
+            'answer': data['translated_text'],
+            'json': data
+        }
+    except Exception as err:
+        # printing stack trace
+        traceback.print_exc()
+        retval = {
+            'answer': translate_message(data.get('lang'), 'Something went wrong, please contact the system administrator.'),
+            'json': {'exception': '%s: %s'%(type(err).__name__, str(err))},
+            'exception': err
+        }
+    return retval
 
-        query = data['translated_text']
+def lfqa_service(form):
+    data = {}
+    try:
+        retval = neamt_service(form, 'lfqa_')
+
+        if retval.get('exception'):
+            raise err
+        
+        data = retval['json']
+        query = retval['answer']
         # call the qa system
-        qa_sys = lfqa_systems[form['qa_system_id']]
+        qa_sys = lfqa_systems[form['lfqa_qa_system_id']]
 
         qa_data = qa_sys['request_formatter'](query, qa_sys['uri'])
 
@@ -83,7 +224,8 @@ def neamt_service(form):
         traceback.print_exc()
         retval = {
             'answer': translate_message(data.get('lang'), 'Something went wrong, please contact the system administrator.'),
-            'json': {'exception': '%s: %s'%(type(err).__name__, str(err))}
+            'json': {'exception': '%s: %s'%(type(err).__name__, str(err))},
+            'exception': True
         }
     return retval
 
@@ -100,8 +242,10 @@ def poolparty_service(form):
     }
 
 services = {
-    'neamt': neamt_service,
+    'lfqa': lfqa_service,
     'poolparty': poolparty_service,
+    'neamt': neamt_service,
+    'mst5': mst5_service,
 }
 
 @app.route('/', methods=['GET', 'POST'])
@@ -109,6 +253,8 @@ def qa():
     data = {
         'poolparty_projects': get_poolparty_projects(),
         'lfqa_systems': lfqa_systems,
+        'porque_langs': porque_langs,
+        'sparql_endpoints': sparql_endpoints
     }
     if 'query' in flask.request.form:
         data.update(services[flask.request.form['service']](flask.request.form))
